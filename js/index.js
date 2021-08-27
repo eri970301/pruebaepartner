e8a84ec693ff557c0321682025dd3cfb5f3e59f4
Erika Otalvaro <erika970301@live.com>
2021-08-27 15:06:09 -0500

    cambios finales1

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//Funcion de accion de botones cuando pasamos el raton por encima de un elemento de "nueva colección"
+
 var accion = document.getElementById("coleccion"); document.getElementById("botoncar")
 
 accion.onmouseover = function(e){
