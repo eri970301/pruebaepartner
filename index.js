d3dba23572f47c55f119984a500cbbbea1b46051
Erika Otalvaro <erika970301@live.com>
2021-08-27 15:03:06 -0500

    cambios finales

---
+++ index.js
@@ -0,0 +1,5 @@
+var accion = document.getElementById("coleccion"); document.getElementById("botoncar")
+
+accion.onmouseover = function(e){
+    document.getElementById("botoncar").style.display = "block";
+}
